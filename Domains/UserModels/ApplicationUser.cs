7e0145d55f9de6b9a9c709045dba5d430450f4ba
Hamdy <hamdymohamed2651@gmail.com>
2025-11-19 13:25:47 +0200

    upate on master

--- Domains/UserModels/ApplicationUser.cs
+++ Domains/UserModels/ApplicationUser.cs
@@ -13,6 +13,7 @@ namespace InfrastructureLayer.UserModels
     public class ApplicationUser : IdentityUser<Guid>
     {
         public string? Name { get; set; } = string.Empty;
+        public int CurrentState { get; set; } = 0;
         public ICollection<UserSubscription> Subscriptions { get; set; } = new List<UserSubscription>();
         public ICollection<UserProfile> Profiles { get; set; } = new List<UserProfile>();
 
--- Presentation/Controllers/AuthController.cs
+++ Presentation/Controllers/AuthController.cs
@@ -81,13 +81,31 @@ namespace Presentation.Controllers
         // ============================
         // Confirm Email & Generate Tokens
         // ============================
+        //[HttpPost("confirm-signup")]
+        //public async Task<ActionResult<ApiResponse<LoginResponseDto>>> ConfirmSignUp([FromBody] ConfirmMagicLinkDto dto)
+        //{
+        //    if (!ModelState.IsValid)
+        //        return BadRequest(ApiResponse<LoginResponseDto>.FailResponse("Invalid input"));
+
+        //    var user = await _authService.ConfirmEmailAndGenerateTokensAsync(dto.UserId, dto.Token);
+        //    if (user == null)
+        //        return BadRequest(ApiResponse<LoginResponseDto>.FailResponse("Invalid or expired token."));
+
+        //    SetRefreshTokenCookie(user.RefreshToken);
+        //    return Ok(ApiResponse<LoginResponseDto>.SuccessResponse(user, "Email confirmed and user logged in."));
+        //}
+
+        // ============================
+        // Register & Generate Tokens
+        // ============================
+
         [HttpPost("confirm-signup")]
         public async Task<ActionResult<ApiResponse<LoginResponseDto>>> ConfirmSignUp([FromBody] ConfirmMagicLinkDto dto)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ApiResponse<LoginResponseDto>.FailResponse("Invalid input"));
 
-            var user = await _authService.ConfirmEmailAndGenerateTokensAsync(dto.UserId, dto.Token);
+            var user = await _authService.ConfirmEmailAndGenerateTokensAsync(dto.Email, dto.Token);
             if (user == null)
                 return BadRequest(ApiResponse<LoginResponseDto>.FailResponse("Invalid or expired token."));
 
@@ -95,9 +113,7 @@ namespace Presentation.Controllers
             return Ok(ApiResponse<LoginResponseDto>.SuccessResponse(user, "Email confirmed and user logged in."));
         }
 
-        // ============================
-        // Register & Generate Tokens
-        // ============================
+
         [HttpPost("register")]
         public async Task<ActionResult<ApiResponse<LoginResponseDto>>> Register([FromBody] RegisterDto dto)
         {
